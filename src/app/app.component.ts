2b504475bc1141b2fb476b2f87bad958194c715e
Kolawole <omotoshok9@gmail.com>
2023-05-30 10:26:55 +0100

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { ApiService } from './services/api.service';
-import {MatPaginator } from '@angular/material/paginator'
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -12,7 +12,14 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  displayedColumns: string[] = ['productName', "category", 'price', 'comment', 'freshness', 'date' ]
+  displayedColumns: string[] = [
+    'productName',
+    'category',
+    'price',
+    'freshness',
+    'date',
+    'comment',
+  ];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -35,18 +42,18 @@ export class AppComponent implements OnInit {
       next: (res) => {
         this.dataSource = new MatTableDataSource(res);
         this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort
+        this.dataSource.sort = this.sort;
       },
       error: (err) => {
         alert('Erro while fetching the Records!');
       },
     });
   }
-  applyFilter(event: Event){
-    const filterValue = (event.target as HTMLInputElement).value
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue;
 
-    if (this.dataSource.paginator){
+    if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
   }
