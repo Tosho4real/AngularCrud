50a8ee3b46cb2929be3542e5bf21d5ab1f4e322f
Kolawole <omotoshok9@gmail.com>
2023-05-30 09:52:32 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'AngularCrud';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['productName', "category", 'price', 'comment', 'freshness', 'date' ]
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%',
+    });
+  }
+
+  getAllProducts() {
+    this.api.getProducts().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort
+      },
+      error: (err) => {
+        alert('Erro while fetching the Records!');
+      },
+    });
+  }
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue;
+
+    if (this.dataSource.paginator){
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
